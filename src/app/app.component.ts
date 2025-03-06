ee4d0849140f756cc310c62b24be0309ba0a2ccc
Thet Paing Soe <thet.paingsoe@yomabank.com>
2025-03-06 15:46:39 +0630

    first upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'invitation';
+
+
+    open() {
+      const element = document.getElementById('envelope');
+      
+      if (element) {
+        element.classList.add('open');
+        element.classList.remove('close');
+      }
+
+      if(element){
+        setTimeout(() => {
+          element.classList.add('my-class');
+        }, 2000); 
+
+      }
+      const cover = document.getElementById('cover');
+      const envlopeWrapper = document.getElementById('envlope-wrapper');
+
+      if(envlopeWrapper) {
+        setTimeout(() => {
+          envlopeWrapper.classList.add('hide');
+        }, 3000); // Delay of 2 seconds
+
+        setTimeout(() => {
+          envlopeWrapper.classList.add('hide2');
+        }, 3500); // Delay of 2 seconds
+    }
+
+      if(cover) {
+        setTimeout(() => {
+          cover.classList.add('show');
+
+        }, 3500); // Delay of 2 seconds
+     }
+    }
+
+    close() {
+      const element = document.getElementById('envelope');
+      if (element) {
+        element.classList.add('close');
+        element.classList.remove('open');
+
+      }
+    }
+
+
+    playSound() {
+      let audio = new Audio();
+      audio.src = '../assets/turn.mp3'; // Path to your sound file
+      audio.load();
+      audio.play();
+    }
 }
