2b967d8175bdfc16a81b5c5c084537deaa12ee12
puyitt <thetpaingsoe.bece@gmail.com>
2025-03-16 00:46:47 +0630

    update image real

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component,ViewChildren, QueryList, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'invitation';
 
 
@@ -40,5 +40,42 @@ export class AppComponent {
       audio.src = '../assets/turn.mp3'; // Path to your sound file
       audio.load();
       audio.play();
+    }
+
+    @ViewChildren('page') pages!: QueryList<ElementRef>;
+
+    images: string[] = [
+      '../assets/1.JPG',
+      '../assets/2.JPG',
+      '../assets/3.JPG',
+      '../assets/4.JPG'
+    ];
+
+    ngAfterViewInit() {
+      const pagesArray = this.pages.toArray().map(el => el.nativeElement);
+
+      // Set zIndex for even pages
+      pagesArray.forEach((page, i) => {
+        if (i % 2 === 0) {
+          page.style.zIndex = (pagesArray.length - i).toString();
+        }
+        page.dataset.pageNum = (i + 1).toString();
+      });
+    }
+
+    onPageClick(event: Event) {
+      const page = event.target as HTMLElement | null;
+    
+      if (!page) return; // Ensure it's not null
+    
+      const pageNum = Number(page.dataset['pageNum']);
+    
+      if (pageNum % 2 === 0) {
+        page.classList.remove('flipped');
+        (page.previousElementSibling as HTMLElement)?.classList.remove('flipped');
+      } else {
+        page.classList.add('flipped');
+        (page.nextElementSibling as HTMLElement)?.classList.add('flipped');
+      }
     }
 }
